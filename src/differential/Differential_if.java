c1b86ea12ea1385143b23be6add0c6b57f577002
50516025 <my426181@ipc.shizuoka.ac.jp>
2016-11-07 17:53:34 +0900

    20161107 submit

---
+++ src/differential/Differential_if.java
@@ -0,0 +1,8 @@
+package differential;
+
+public interface Differential_if {
+	
+	public double getRx();//理論値を返す関数
+	public double getDx();//数値微分値を返す関数
+
+}
---
+++ src/differential/Differential_main.java
@@ -0,0 +1,31 @@
+//Input value of  a> 1
+//Input value of h> 0.00000000001
+//f(x)=x^2‚Ìx=1.0‚É‚¨‚¯‚é”÷•ªŒW”‚Í2.0
+//f(x)=x^2‚Ìx=1.0‚É‚¨‚¯‚é”’lÏ•ª‚Í2.000000165480742
+//‘Š‘ÎŒë·=8.274037099909037E-6
+
+package differential;
+
+import java.util.Scanner;
+
+
+public class Differential_main {
+	public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	System.out.print("Input value of  a> ");
+	double a = Double.parseDouble(scan.next());
+	System.out.print("Input value of h> ");
+	double h = Double.parseDouble(scan.next());
+	
+	Differential_lib dlib = new Differential_lib(a,h);
+	System.out.println("f(x)=x^2‚Ìx="+a+"‚É‚¨‚¯‚é”÷•ªŒW”‚Í"+dlib.getRx());
+	
+	Differential_lib dlib1 = new Differential_lib(a,h);
+	System.out.println("f(x)=x^2‚Ìx="+a+"‚É‚¨‚¯‚é”’lÏ•ª‚Í"+dlib1.getDx());
+	
+	Differential_lib clib = new Differential_lib(a,h);
+	System.out.println("‘Š‘ÎŒë·="+clib.getAns());
+	}
+
+
+}
